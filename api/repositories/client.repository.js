2c57f55cacf788e08a047d00e7fe009997bd7f56
Angelo Rafael <angelorpt@gmail.com>
2021-09-07 19:54:33 -0400

    upd | Liberando a conexão com o banco

--- api/repositories/client.repository.js
+++ api/repositories/client.repository.js
@@ -2,18 +2,24 @@ import { connect } from "./database";
 
 const insertClient = async (client) => {
   const conn = await connect();
-  const sql = `INSERT INTO clients (name, cpf, phone, email, address) 
-                            values ($1, $2, $3, $4, $5)
-               RETURNING *`;
-  const values = [
-    client.name,
-    client.cpf,
-    client.phone,
-    client.email,
-    client.address,
-  ];
-  const result = await conn.query(sql, values);
-  return result.rows[0];
+  try {
+    const sql = `INSERT INTO clients (name, cpf, phone, email, address) 
+                              values ($1, $2, $3, $4, $5)
+                 RETURNING *`;
+    const values = [
+      client.name,
+      client.cpf,
+      client.phone,
+      client.email,
+      client.address,
+    ];
+    const result = await conn.query(sql, values);
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  } finally {
+    conn.release();
+  }
 };
 
 export default { insertClient };

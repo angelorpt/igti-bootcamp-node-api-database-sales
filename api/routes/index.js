a5479a4fdc1156af00f6e1c1976396f539fccfb3
Angelo Rafael <angelorpt@gmail.com>
2021-09-07 16:09:16 -0400

    add | Configuração de Logs e Rotas Iniciais

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,13 @@
 import clientsRoutes from "./clients";
+import suppliesRoutes from "./supplies";
+import productsRoutes from "./products";
+import salesRoutes from "./sales";
 
 const routes = (app) => {
   app.use("/api/clients", clientsRoutes);
+  app.use("/api/supplies", suppliesRoutes);
+  app.use("/api/products", productsRoutes);
+  app.use("/api/sales", salesRoutes);
 };
 
 export default routes;
--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,21 @@
-import express from "express";
+import express, { application } from "express";
+import cors from "cors";
 import routes from "../routes";
+import configureLog from "./log";
+
+configureLog();
 
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 routes(app);
 
-app.listen(process.env.PORT);
+app.get("/", (req, res) => res.send("okok"));
+
+app.listen(process.env.PORT, () => console.log("API Running"));
 
 export default app;
